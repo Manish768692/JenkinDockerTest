0ee2afa4b594c55637440d544aaeea0ae0d55cf7
Manishh <mkumar7686@gmail.com>
2026-03-26 12:12:53 +0530

    Initial commit

---
+++ src/main/java/com/mn/JenkinDockerTestApplication.java
@@ -0,0 +1,13 @@
+package com.mn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinDockerTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinDockerTestApplication.class, args);
+	}
+
+}
